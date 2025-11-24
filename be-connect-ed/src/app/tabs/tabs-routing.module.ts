8dea484957aa5d8e63461da72164fb4527bd4c35
Kimmypara <kparascandalo@gmail.com>
2025-11-24 20:07:59 +0100

    Background
    Added icons
    background image
    logo

--- be-connect-ed/src/app/tabs/tabs-routing.module.ts
+++ be-connect-ed/src/app/tabs/tabs-routing.module.ts
@@ -19,6 +19,11 @@ const routes: Routes = [
         path: 'tab3',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
+        {
+        path: 'notifications',
+        loadChildren: () => import('../notifications/notifications.module').then(m => m.NotificationsPageModule)
+      },
+
       {
         path: '',
         redirectTo: '/tabs/tab1',
