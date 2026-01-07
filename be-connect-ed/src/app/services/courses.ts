57696097e4a2393dbe9f4d47f47f3fc6dfea7f87
Kimmypara <kparascandalo@gmail.com>
2026-01-07 17:56:44 +0100

    Added services for courses

---
+++ be-connect-ed/src/app/services/courses.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Courses {
+  course_id='';
+  course_code='';
+  course_description='';
+  course_name='';
+  course_
+}
